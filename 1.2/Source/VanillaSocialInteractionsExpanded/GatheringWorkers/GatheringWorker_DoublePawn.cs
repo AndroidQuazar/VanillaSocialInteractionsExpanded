b3f7bbbaea278b8d431a91b8ce778ab1f446dbc4
Taranchuk <nfhfyxer@gmail.com>
2021-03-10 16:27:10 +0200

    update

--- 1.2/Source/VanillaSocialInteractionsExpanded/GatheringWorkers/GatheringWorker_DoublePawn.cs
+++ 1.2/Source/VanillaSocialInteractionsExpanded/GatheringWorkers/GatheringWorker_DoublePawn.cs
@@ -17,38 +17,49 @@ namespace VanillaSocialInteractionsExpanded
 
         public override bool CanExecute(Map map, Pawn organizer = null)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - if (organizer == null) - 1", true);
             if (organizer == null)
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - organizer = FindOrganizerCustom(map, out var companion); - 2", true);
                 organizer = FindOrganizerCustom(map, out var companion);
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - if (organizer is null) - 3", true);
                 if (organizer is null)
                 {
                     Log.Message($"{this.def.defName} - organizer isn't found");
+                    Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 5", true);
                     return false;
                 }
                 else if (companion is null)
                 {
                     Log.Message($"{this.def.defName} - companion isn't found");
+                    Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 8", true);
                     return false;
                 }
             }
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - if (!TryFindGatherSpot(organizer, out IntVec3 _)) - 9", true);
             if (!TryFindGatherSpot(organizer, out IntVec3 _))
             {
                 Log.Message($"{this.def.defName} - unable to find gather spot");
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 11", true);
                 return false;
             }
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - if (!GatheringsUtility.PawnCanStartOrContinueGathering(organizer)) - 12", true);
             if (!GatheringsUtility.PawnCanStartOrContinueGathering(organizer))
             {
                 Log.Message($"{this.def.defName} - {organizer} can't start or continue gathering");
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 14", true);
                 return false;
             }
             else if (FindCompanion(organizer, this.def) is null)
             {
                 Log.Message($"{this.def.defName} - can't find a companion for {organizer}");
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 17", true);
                 return false;
             }
             else if (!ConditionsMeet(organizer))
             {
                 Log.Message($"{this.def.defName} - outside conditions aren't meet");
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CanExecute - return false; - 20", true);
                 return false;
             }
             return true;
@@ -56,32 +67,47 @@ namespace VanillaSocialInteractionsExpanded
 
         public override bool TryExecute(Map map, Pawn organizer = null)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - Pawn companion = null; - 22", true);
             Pawn companion = null;
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - if (organizer == null) - 23", true);
             if (organizer == null)
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - organizer = FindOrganizerCustom(map, out companion); - 24", true);
                 organizer = FindOrganizerCustom(map, out companion);
             }
 
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - if (organizer == null) - 25", true);
             if (organizer == null)
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - return false; - 26", true);
                 return false;
             }
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - if (!TryFindGatherSpot(organizer, out IntVec3 spot)) - 27", true);
             if (!TryFindGatherSpot(organizer, out IntVec3 spot))
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - return false; - 28", true);
                 return false;
             }
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - if (!GatheringsUtility.PawnCanStartOrContinueGathering(organizer)) - 29", true);
             if (!GatheringsUtility.PawnCanStartOrContinueGathering(organizer))
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - return false; - 30", true);
                 return false;
             }
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - if (organizer is null || companion is null || !ConditionsMeet(organizer)) - 31", true);
             if (organizer is null || companion is null || !ConditionsMeet(organizer))
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - return false; - 32", true);
                 return false;
             }
             LordJob lordJob = CreateLordJobCustom(spot, organizer, companion);
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - List<Pawn> pawns = new List<Pawn> { organizer, companion }; - 34", true);
             List<Pawn> pawns = new List<Pawn> { organizer, companion };
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - LordMaker.MakeNewLord(organizer.Faction, lordJob, organizer.Map, pawns); - 35", true);
             LordMaker.MakeNewLord(organizer.Faction, lordJob, organizer.Map, pawns);
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - SendLetterCustom(spot, organizer, companion); - 36", true);
             SendLetterCustom(spot, organizer, companion);
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryExecute - return true; - 37", true);
             return true;
         }
 
@@ -92,10 +118,14 @@ namespace VanillaSocialInteractionsExpanded
         }
         private Pawn FindOrganizerCustom(Map map, out Pawn companion)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindOrganizerCustom - var organizer = FindRandomGatheringOrganizer(Faction.OfPlayer, map, def, out companion); - 39", true);
             var organizer = FindRandomGatheringOrganizer(Faction.OfPlayer, map, def, out companion);
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindOrganizerCustom - if (organizer is null) - 40", true);
             if (organizer is null)
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindOrganizerCustom - companion = null; - 41", true);
                 companion = null;
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindOrganizerCustom - return null; - 42", true);
                 return null;
             }
             return organizer;
@@ -106,17 +136,22 @@ namespace VanillaSocialInteractionsExpanded
             var value = pawn.RaceProps.Humanlike && !pawn.InBed() && !pawn.InMentalState && pawn.GetLord() == null
             && GatheringsUtility.ShouldPawnKeepGathering(pawn, gatheringDef) && !pawn.Drafted && (gatheringDef.requiredTitleAny == null || gatheringDef.requiredTitleAny.Count == 0
             || (pawn.royalty != null && pawn.royalty.AllTitlesInEffectForReading.Any((RoyalTitle t) => gatheringDef.requiredTitleAny.Contains(t.def))));
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - BasePawnValidator - return value; - 45", true);
             return value;
         }
         public Pawn FindRandomGatheringOrganizer(Faction faction, Map map, GatheringDef gatheringDef, out Pawn companion)
         {
             Predicate<Pawn> v = (Pawn organizer) => BasePawnValidator(organizer, gatheringDef) && MemberValidator(organizer);// && FindCompanion(organizer, gatheringDef) != null;
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindRandomGatheringOrganizer - if (map.mapPawns.SpawnedPawnsInFaction(faction).Where(x => v(x)).TryRandomElement(out Pawn result)) - 47", true);
             if (map.mapPawns.SpawnedPawnsInFaction(faction).Where(x => v(x)).TryRandomElement(out Pawn result))
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindRandomGatheringOrganizer - companion = FindCompanion(result, gatheringDef); - 48", true);
                 companion = FindCompanion(result, gatheringDef);
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindRandomGatheringOrganizer - return result; - 49", true);
                 return result;
             }
             companion = null;
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindRandomGatheringOrganizer - return null; - 51", true);
             return null;
         }
 
@@ -124,8 +159,10 @@ namespace VanillaSocialInteractionsExpanded
         {
             var candidates = organizer.Map.mapPawns.SpawnedPawnsInFaction(organizer.Faction).Where(candidate => candidate != organizer && BasePawnValidator(candidate, gatheringDef)
                 && MemberValidator(candidate) && PawnsCanGatherTogether(organizer, candidate));
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindCompanion - if (candidates.Any() && candidates.TryRandomElementByWeight(x => SortCandidatesBy(organizer, x), out var companion)) - 53", true);
             if (candidates.Any() && candidates.TryRandomElementByWeight(x => SortCandidatesBy(organizer, x), out var companion))
             {
+                Log.Message("GatheringWorker_DoublePawn : GatheringWorker - FindCompanion - return companion; - 54", true);
                 return companion;
             }
             return null;
@@ -133,31 +170,38 @@ namespace VanillaSocialInteractionsExpanded
 
         protected virtual float SortCandidatesBy(Pawn organizer, Pawn candidate)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - SortCandidatesBy - return 0f; - 56", true);
             return 0f;
         }
         protected virtual LordJob CreateLordJobCustom(IntVec3 spot, Pawn organizer, Pawn companion)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - CreateLordJobCustom - return null; - 57", true);
             return null;
         }
         protected virtual bool MemberValidator(Pawn pawn)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - MemberValidator - return true; - 58", true);
             return true;
         }
         protected virtual bool PawnsCanGatherTogether(Pawn organizer, Pawn companion)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - PawnsCanGatherTogether - return true; - 59", true);
             return true;
         }
         protected virtual bool ConditionsMeet(Pawn organizer)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - ConditionsMeet - return true; - 60", true);
             return true;
         }
         protected override bool TryFindGatherSpot(Pawn organizer, out IntVec3 spot)
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryFindGatherSpot - return RCellFinder.TryFindGatheringSpot_NewTemp(organizer, def, ignoreRequiredColonistCount: false, out spot); - 61", true);
             return RCellFinder.TryFindGatheringSpot_NewTemp(organizer, def, ignoreRequiredColonistCount: false, out spot);
         }
 
         protected override LordJob CreateLordJob(IntVec3 spot, Pawn organizer) // we don't use it, CreateLordJobCustom is used instead with custom arguments;
         {
+            Log.Message("GatheringWorker_DoublePawn : GatheringWorker - TryFindGatherSpot - throw new NotImplementedException(); - 63", true);
             throw new NotImplementedException();
         }
     }
---
+++ 1.2/Source/VanillaSocialInteractionsExpanded/GatheringWorkers/GatheringWorker_MovieNight.cs
@@ -0,0 +1,118 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Grammar;
+
+namespace VanillaSocialInteractionsExpanded
+{
+    public class GatheringWorker_MovieNight : GatheringWorker_Dating
+    {
+        protected override bool ConditionsMeet(Pawn organizer)
+        {
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - ConditionsMeet - return GenLocalDate.HourInteger(organizer.Map) >= 18 && GenLocalDate.HourInteger(organizer.Map) <= 24 && base.ConditionsMeet(organizer); - 1", true);
+            return GenLocalDate.HourInteger(organizer.Map) >= 18 && GenLocalDate.HourInteger(organizer.Map) <= 24 && base.ConditionsMeet(organizer);
+        }
+        protected override bool PawnsCanGatherTogether(Pawn organizer, Pawn companion)
+        {
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - PawnsCanGatherTogether - return base.PawnsCanGatherTogether(organizer, companion) && TryFindTelevisionFor(organizer, companion, out Building television, out IntVec3 spot, out Building chair); - 2", true);
+            return base.PawnsCanGatherTogether(organizer, companion) && TryFindTelevisionFor(organizer, companion, out Building television, out IntVec3 spot, out Building chair);
+        }
+        public static bool TryFindTelevisionFor(Pawn organizer, Pawn companion, out Building television, out IntVec3 spot, out Building chair)
+        {
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - var televisionDefs = DefDatabase<JoyGiverDef>.GetNamed(\"WatchTelevision\").thingDefs; - 3", true);
+            var televisionDefs = DefDatabase<JoyGiverDef>.GetNamed("WatchTelevision").thingDefs;
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - var televisions = organizer.Map.listerBuildings.allBuildingsColonist.Where(x => televisionDefs.Contains(x.def) && CanInteractWith(organizer, x) && CanInteractWith(companion, x)); - 4", true);
+            var televisions = organizer.Map.listerBuildings.allBuildingsColonist.Where(x => televisionDefs.Contains(x.def) && CanInteractWith(organizer, x) && CanInteractWith(companion, x));
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - foreach (var t in televisions) - 5", true);
+            foreach (var t in televisions)
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - if (WatchBuildingUtilityDoublePawn.TryFindBestWatchCell(t, organizer, true, out IntVec3 result, out Building firstChair)) - 6", true);
+                if (WatchBuildingUtilityDoublePawn.TryFindBestWatchCell(t, organizer, true, out IntVec3 result, out Building firstChair))
+                {
+                    Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - if (WatchBuildingUtilityDoublePawn.TryFindBestWatchCellNear(t, companion, firstChair, true, out IntVec3 result2, out Building chair2)) - 7", true);
+                    if (WatchBuildingUtilityDoublePawn.TryFindBestWatchCellNear(t, companion, firstChair, true, out IntVec3 result2, out Building chair2))
+                    {
+                        Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - television = t; - 8", true);
+                        television = t;
+                        Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - spot = result; - 9", true);
+                        spot = result;
+                        Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - chair = firstChair; - 10", true);
+                        chair = firstChair;
+                        Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - return true; - 11", true);
+                        return true;
+                    }
+                    else
+                    {
+                        Log.Message("Failed to find a cell for companion: " + companion);
+                    }
+                }
+                else
+                {
+                    Log.Message("Failed to find a television for organizer: " + organizer);
+                }
+            }
+            television = null;
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - spot = IntVec3.Invalid; - 15", true);
+            spot = IntVec3.Invalid;
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - chair = null; - 16", true);
+            chair = null;
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindTelevisionFor - return false; - 17", true);
+            return false;
+        }
+
+        private static bool CanInteractWith(Pawn pawn, Thing t)
+        {
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - if (!pawn.CanReserve(t)) - 18", true);
+            if (!pawn.CanReserve(t))
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - return false; - 19", true);
+                return false;
+            }
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - if (t.IsForbidden(pawn)) - 20", true);
+            if (t.IsForbidden(pawn))
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - return false; - 21", true);
+                return false;
+            }
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - if (!t.IsSociallyProper(pawn)) - 22", true);
+            if (!t.IsSociallyProper(pawn))
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - return false; - 23", true);
+                return false;
+            }
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - if (!t.IsPoliticallyProper(pawn)) - 24", true);
+            if (!t.IsPoliticallyProper(pawn))
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - return false; - 25", true);
+                return false;
+            }
+            CompPowerTrader compPowerTrader = t.TryGetComp<CompPowerTrader>();
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - if (compPowerTrader != null && !compPowerTrader.PowerOn) - 27", true);
+            if (compPowerTrader != null && !compPowerTrader.PowerOn)
+            {
+                Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - CanInteractWith - return false; - 28", true);
+                return false;
+            }
+            return true;
+        }
+        protected override bool TryFindGatherSpot(Pawn organizer, out IntVec3 spot)
+        {
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindGatherSpot - spot = IntVec3.Invalid; - 30", true);
+            spot = IntVec3.Invalid;
+            Log.Message("GatheringWorker_MovieNight : GatheringWorker_Dating - TryFindGatherSpot - return true; - 31", true);
+            return true;
+        }
+
+        protected override LordJob CreateLordJobCustom(IntVec3 spot, Pawn organizer, Pawn companion)
+        {
+            return new LordJob_Joinable_MovieNight(organizer, companion, spot, this.def, new IntRange(2 * GenDate.TicksPerHour, 4 * GenDate.TicksPerHour).RandomInRange);
+        }
+    }
+}
